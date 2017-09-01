cb798e424e2b253866a5ae51909ab0d82b685f77
yangm <yangmin@gionee.com>
2017-09-01 17:24:14 +0800

    添加省信息获取和显示

--- app/src/main/java/com/example/niezhenzhen/coolweather/service/ChooseAreaFragment.java
+++ app/src/main/java/com/example/niezhenzhen/coolweather/service/ChooseAreaFragment.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,14 +14,20 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.niezhenzhen.coolweather.R;
+import com.example.niezhenzhen.coolweather.db.Province;
 import com.example.niezhenzhen.coolweather.gson.GsonUtil;
 import com.example.niezhenzhen.coolweather.gson.HttpUtil;
 
+import org.litepal.crud.DataSupport;
+
+import java.io.DataOutput;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -35,16 +42,16 @@ public class ChooseAreaFragment extends Fragment implements View.OnClickListener
 
     private GsonUtil mGsonUtil;
     private String chinaUrl = "http://guolin.tech/api/china";
-    ListView provinceList;
+    ListView provinceListView;
     RecyclerView cityRecy;
     Button otherCityBtn;
     Button hotCityBtn;
     View provinceFragment;
     View chooseCity;
+    boolean provinceIsVisiable = false;
     ArrayList<String> hostCitys = new ArrayList<>();
     String[] cityArr = {"北京","上海","广州","深圳","武汉","长沙","成都","重庆","大连","哈尔滨","杭州","济南","昆明","合肥","青岛","三亚","天津","西安","郑州","厦门","香港"};
     ArrayList<String> provinces = new ArrayList<>();
-    String[] provinceArr = {"北京","上海","广州","深圳","武汉","长沙","成都","重庆","大连","哈尔滨","杭州","济南","昆明","合肥"};
 
     @Nullable
     @Override
@@ -53,13 +60,14 @@ public class ChooseAreaFragment extends Fragment implements View.OnClickListener
         if(hostCitys.isEmpty()){
             getList(cityArr);
         }
-        provinceList = (ListView) chooseCity.findViewById(R.id.province_list);
+        provinceListView = (ListView) chooseCity.findViewById(R.id.province_list);
         cityRecy = (RecyclerView) chooseCity.findViewById(R.id.city_content);
         otherCityBtn = (Button) chooseCity.findViewById(R.id.other_city);
         hotCityBtn = (Button) chooseCity.findViewById(R.id.hot_city);
         provinceFragment = chooseCity.findViewById(R.id.province_frame);
 
         otherCityBtn.setOnClickListener(this);
+        hotCityBtn.setOnClickListener(this);
         cityRecy.setLayoutManager(new GridLayoutManager(getActivity(),3));
         cityRecy.setAdapter(new CityAdapter());
         return chooseCity;
@@ -76,36 +84,70 @@ public class ChooseAreaFragment extends Fragment implements View.OnClickListener
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.other_city:
-                provinceFragment.setVisibility(View.VISIBLE);
-                if(provinces.isEmpty()){
-                    for (String str:provinceArr) {
-                        provinces.add(str);
-                    }
+                if(readProvinceFromDB()&&!provinceIsVisiable){
+                    provinceFragment.setVisibility(View.VISIBLE);
+                    provinceIsVisiable = true;
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,provinces);
+                    provinceListView.setAdapter(arrayAdapter);
                 }
-                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,provinces);
-                provinceList.setAdapter(arrayAdapter);
                 break;
             case R.id.hot_city:
-
+                if(provinceIsVisiable){
+                    Log.i(G.TAG,"隐藏列表");
+                    provinceFragment.setVisibility(View.GONE);
+                    provinceIsVisiable = false;
+                }
                 break;
         }
     }
 
-    private void getProvinceData(){
+    /**
+     * 从网络下省数据
+     */
+    private void getProvinceDataFromNet(){
         HttpUtil.sendHttpRequest(chinaUrl, new Callback() {
             @Override
             public void onFailure(Call call, IOException e) {
-
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getActivity(),"读取数据失败",Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
 
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                if(response.isSuccessful()){
-                    GsonUtil gsonUtil = new GsonUtil();
-                    gsonUtil.parseJsonToProvince(response.message());
-                }
+                GsonUtil gsonUtil = new GsonUtil();
+                gsonUtil.parseJsonToProvince(response.body().string());
             }
         });
+    }
+
+    /**
+     * 读取数据库中的省信息，添加数数据列表
+     * @return 读取到数据返回true
+     */
+    public boolean readProvinceFromDB(){
+        List<Province> provinceData = DataSupport.findAll(Province.class);
+        if(provinceData.isEmpty()){
+
+            getProvinceDataFromNet();
+            provinceData = DataSupport.findAll(Province.class);
+
+            if(provinceData.isEmpty()){
+                /*数据库是空的，没有数据*/
+                return false;
+            }
+            for (Province p:provinceData) {
+                provinces.add(p.getProvinceName());
+            }
+        }else{
+            for (Province p:provinceData) {
+                provinces.add(p.getProvinceName());
+            }
+        }
+        return true;
     }
 
     /**
